e829c79d140fc13eb2e1488ebd2cdfe85b285917
ucantfindme <8765s.p.krishna.n@gmail.com>
2022-07-02 16:36:43 +0530

    ..

--- Assignment Problem/TA Assignment Problem/CourseOptimal.cpp
+++ Assignment Problem/TA Assignment Problem/CourseOptimal.cpp
@@ -55,13 +55,17 @@ class Student{
 
 //Validate module
 void validate(int n,int m,map<string,Course>&coursemap,map<string,map<string,int> >&courseStudentrank,map<string,vector<tuple<int,int,string> > >&coursestudentpriority,map<string,vector<tuple<int,string> > >&studentcoursepriority,map<string,string>&assigned){
+    //This variable will be counter for number of students who are part of unstable assignment
     int t=0;
     for(auto stu:studentcoursepriority){
         string student=stu.first;
+        //we reverse the priority list to get highest priority at the front
         reverse(studentcoursepriority[student].begin(),studentcoursepriority[student].end());
+        //we run a loop till the student's assigned course
         for(auto j: studentcoursepriority[student]){
             string course=get<1>(j);
             if(course!=assigned[student]){
+                //Here we check the priority rank of the student in the preference list of that course and the closing rank of that course
                 if(n-coursestudentpriority[course].size()>courseStudentrank[course][student]){
                     t++;
                     break;
@@ -70,28 +74,37 @@ void validate(int n,int m,map<string,Course>&coursemap,map<string,map<string,int
             else break;
         }
     }
+    //If t=0 that shows there's no unstable assignement
     if(t==0)cout<<"Output Validated Successfully!\n";
     else cout<<"Number of unstable assignments: "<<t<<"\n";
 }
 
+//Course-Optimal Assignment
 void assignCourseOptimal(int n,int m,map<string,Course>&coursemap,map<string,Student>&studentmap,map<string,vector<tuple<int,int,string> > >&coursestudentpriority,map<string,vector<tuple<int,string> > >&studentcoursepriority,map<string,map<string,tuple<int,int,string> > >&coursestudentprioritymap,map<string,map<string,tuple<int,string> > >&studentcourseprioritymap){
+    //creating a course queue, initially containing all courses
     queue<string>coursequeue;
     for(auto course:coursestudentpriority)coursequeue.push(course.first);
+    //Until there are no courses left
     while(!coursequeue.empty()){
         string course=coursequeue.front();
+        //if course has filled all the seats or it has offered to all the students, we can skip it
         if(coursemap[course].assignedTAs==coursemap[course].tasReq || coursestudentpriority[course].empty()){
             coursequeue.pop();
             continue;
         }
+        //get the highest preferred student on the course-student priority list 
         string student=get<2>(*coursestudentpriority[course].rbegin());
         coursestudentpriority[course].pop_back();
+        //if the student is not assigned to any course
         if(studentmap[student].assigned=="-"){
             coursemap[course].assignedTAs++;
             studentmap[student].assigned=course;
         }
         else{
             string presentcourse=studentmap[student].assigned;
+            //Here we check if the assigned course of this student is less prior to the course making offer
             if(studentcourseprioritymap[student][course]>studentcourseprioritymap[student][presentcourse]){
+                //if yes we assign the course to the student and take back previously assigned course into our course queue 
                 coursemap[course].assignedTAs++;
                 coursemap[presentcourse].assignedTAs--;
                 studentmap[student].assigned=course;
@@ -99,6 +112,7 @@ void assignCourseOptimal(int n,int m,map<string,Course>&coursemap,map<string,Stu
             }
         }
     }
+    //Writing our assignment into a CSV file
     fstream fout1;
     fout1.open("myoutput.csv",ios::out);
     fout1<<"S.No.,Name,Roll No,Email,Degree,Course Allocated\n";
@@ -107,6 +121,7 @@ void assignCourseOptimal(int n,int m,map<string,Course>&coursemap,map<string,Stu
     fout1.close();
 }
 
+//Given a csv filename this module returns a 2D vector of csv contents
 vector<vector<string> > getfilecontents(string fname){
     vector<vector<string> > content;
     vector<string> row;
@@ -126,18 +141,19 @@ vector<vector<string> > getfilecontents(string fname){
 }
 
 int main(){
+    //load input 
     string coursefile = "input2_new.csv";
     string studentfile = "input1_new.csv";
     vector<vector<string> >coursedata,studentdata;
     coursedata=getfilecontents(coursefile);
     studentdata=getfilecontents(studentfile);
 
+    //creating course objects from course records read from csv and storing them into a map with courseid as the key
     map<string,Course> coursemap;
     for(int i=1;i<coursedata.size();i++){
         string id=coursedata[i][2];
         if(id=="-")continue;
         int maxTAs=atoi(&coursedata[i][4][0]);
-        //cout<<id<<" "<<maxTAs<<"\n";
         if(maxTAs<1)continue;
         vector<string> prefTAList;
         for(int j=5;j<15;j++){
@@ -154,6 +170,7 @@ int main(){
         coursemap[id] = * new Course(id,maxTAs,prefTAList,prefProgList,n,m);
     }
 
+    //creating student objects from student records read from csv and storing them into a map with student rollno as the key
     map<string,Student>studentmap;
     for(int i=1;i<studentdata.size();i++){
         string id=studentdata[i][1];
@@ -167,15 +184,26 @@ int main(){
         }
         int n=prefCou.size();
         studentmap[id]=*new Student(name,email,id,deg,prefCou,n);
-        //cout<<id<<"\n";
     }
     
     int n=studentmap.size(),m=coursemap.size();
+
+    //A map with courseid as key and value a vector containing the priorities of each student of that course
     map<string,vector<tuple<int,int,string> > >coursestudentpriority;
+
+    //A map with studentid as key and value a vector containing the priorities  of each course of that student
     map<string,vector<tuple<int,string> > >studentcoursepriority;
+
+    //this variable contains course's priority of the students(all) and storing them into nested map
     map<string,map<string,tuple<int,int,string> > >coursestudentprioritymap;
+
+    //this variable contains student's priority of the courses(all) and storing them into nested map
     map<string,map<string,tuple<int,string> > >studentcourseprioritymap;
+
+    //A nested map to get the rank of a student in the preferrence of a course
     map<string,map<string,int> >courseStudentrank;
+
+    //Assigning priorities to each possible pair of student and course and storing them
     for(auto student:studentmap){
         for(auto course:coursemap){
             int p1=-1000,p2=-1000;
@@ -204,21 +232,22 @@ int main(){
             studentcourseprioritymap[student.first][course.first]=make_tuple(p1,course.first);
         }
     }
-    for(auto student:studentcoursepriority){
-        sort(studentcoursepriority[student.first].begin(),studentcoursepriority[student.first].end());
-    }
-    for(auto course:coursestudentpriority){
-        sort(coursestudentpriority[course.first].begin(),coursestudentpriority[course.first].end());
-    }
+    
+    //sorting them to get a ordered priorities
+    for(auto student:studentcoursepriority)sort(studentcoursepriority[student.first].begin(),studentcoursepriority[student.first].end());
+    for(auto course:coursestudentpriority)sort(coursestudentpriority[course.first].begin(),coursestudentpriority[course.first].end());
+    
+    //Assigning ranks
     for(auto course:coursemap){
         int rk=0;
-        for(auto student:coursestudentpriority[course.first]){
-            courseStudentrank[course.first][get<2>(student)]=n-rk++;
-        }
+        for(auto student:coursestudentpriority[course.first])courseStudentrank[course.first][get<2>(student)]=n-rk++;
     }
 
+    //Calling Assignment Module
     assignCourseOptimal(n,m,coursemap,studentmap,coursestudentpriority,studentcoursepriority,coursestudentprioritymap,studentcourseprioritymap);
 
+    //Validation
+    //Load ouputs from csv
     string testoutputfile = "output.csv";
     string myoutputfile = "myoutput.csv";
     vector<vector<string> >testout,myout;
@@ -231,9 +260,11 @@ int main(){
         mymap[myout[i][2]]=myout[i][5];
     }
     
+    //validate by checking if there are any unstable assignment
     validate(n,m,coursemap,courseStudentrank,coursestudentpriority,studentcoursepriority,mymap);
     //validate(n,m,coursemap,courseStudentrank,coursestudentpriority,studentcoursepriority,testmap);
 
+    //Write the requirement and assigned number of TAs to each course onto a CSV file
     fstream fout1;
     fout1.open("mycount.csv",ios::out);
     fout1<<"S.No.,Course,#TAs Requirement,#TAs Assigned\n";
